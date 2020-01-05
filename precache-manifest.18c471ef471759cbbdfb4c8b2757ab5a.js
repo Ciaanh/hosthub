fd74a81c23765344bf672acef82c661692d33fdb
Ciaanh <nicolas.fabre.21@outlook.fr>
2020-01-05 22:54:07 +0100

    Updates

---
+++ precache-manifest.18c471ef471759cbbdfb4c8b2757ab5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0d34fb0f616f4732a1a6812910c387",
+    "url": "/hosthub/index.html"
+  },
+  {
+    "revision": "ec45db863e53b873c25d",
+    "url": "/hosthub/static/css/main.4876ecdd.chunk.css"
+  },
+  {
+    "revision": "4f8949287c9bbd65bf25",
+    "url": "/hosthub/static/js/2.08be2d3c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hosthub/static/js/2.08be2d3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec45db863e53b873c25d",
+    "url": "/hosthub/static/js/main.4938f31e.chunk.js"
+  },
+  {
+    "revision": "ec0a8f76c61382b7aa48",
+    "url": "/hosthub/static/js/runtime-main.812b3d71.js"
+  }
+]);
